fb8a68505292078de2e8c1b146b2ab44576990d1
Bizhou Duan <bizhou.duan@gmail.com>
2021-04-30 16:33:58 -0700

    add restconf support

--- create_device_restconf.py
+++ create_device_restconf.py
@@ -1,40 +1,38 @@
-#!/usr/bin/env python
-
-import json
+# This module will add new device to the Cisco NSO using restconf
+# Version: 1.0
+# Author: Bizhou Duan
+# Copyright 2021, Sparklight ®. All rights reserved
 import requests
+from requests.auth import HTTPBasicAuth
+from xml.dom import minidom
 
-TheDevice = {
-    "device": {
-        "name": "ios3",
-        "address": "127.0.0.1",
-        "port": 10022,
-        "state": {
-            "admin-state": "unlocked"
-        },
-        "authgroup": "default",
-        "device-type": {
-            "cli": {
-                "ned-id": "tailf-ned-cisco-ios-id:cisco-ios"
-            }
-        }
-    }
-}
+# Open the xml config file
+with open('device.xml', 'r') as f:
+    result = f.read()
+
+# Get the hostname from the xml file
+myxml = minidom.parse('device.xml')
+name = myxml.getElementsByTagName('name')
+hostname = name[0].firstChild.data
 
 
 def main():
-    baseUri = "http://localhost:8080/restconf/data"
-    auth = ('admin', 'admin')
-    headers = {'Content-Type': 'application/yang-data+json'}
-    resp = requests.put(baseUri + '/devices/device=ios3', auth=auth, headers=headers,
-                        data=json.dumps(TheDevice))
-    print(resp)
+    base_uri = "http://10.140.63.11:8080/restconf/data/tailf-ncs:devices"
+    auth = HTTPBasicAuth('autoeng', 'AHrz3ANhfLESs44')
+    headers = {'Accept': 'application/yang-data+json',
+               'Content-Type': 'application/yang-data+xml'}
 
-    baseUriOper = "http://localhost:8080/restconf/operations"
-    resp = requests.post(baseUriOper + "/devices/device=ios3/ssh/fetch-host-keys", auth=auth, headers=headers)
-    print(resp)
-
-    resp = requests.post(baseUriOper + "/devices/device=ios3/sync-from", auth=auth, headers=headers)
-    print(resp)
+    # Create a Device in the CDB
+    response = requests.post(base_uri, auth=auth, headers=headers, data=result, verify=False)
+    print(response)
+    # SSH Fetch-Host-Keys Device
+    response = requests.post(base_uri + "/device=" + hostname + "/ssh/fetch-host-keys",
+                             auth=auth, headers=headers, verify=False)
+    print(response)
+    # Sync-from Device
+    response = requests.post(base_uri + "/device=" + hostname + "/sync-from",
+                             auth=auth, headers=headers, verify=False)
+    print(response)
 
 
 if __name__ == "__main__":
